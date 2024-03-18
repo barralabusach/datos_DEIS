16b64f84c6f801c70f357e127f995a3565a9a5a9
barralabusach <143545017+barralabusach@users.noreply.github.com>
2024-03-18 16:01:16 -0300

    Add files via upload

--- 05-descriptive.R
+++ 05-descriptive.R
@@ -16,21 +16,68 @@ str(loc)
 summary(data)
 plot(data$total_admission)
 
-# Select data of myocardial infarction:
-data_2 <- subset(data_2, admission_id == 13)
-data_2$year <- as.numeric(substr(data_2$date, 1, 4))
-data_2$month <- as.numeric(substr(data_2$date, 6, 7))
-str(data_2)
+# Select data of "myocardial infarction" with code #13:
+myocar <- subset(data, admission_id == 13)
+myocar$year <- as.numeric(substr(myocar$date, 1, 4))
+myocar$month <- as.numeric(substr(myocar$date, 6, 7))
+str(myocar)
 
-data_3 <- aggregate(data_2$total_admission, by=list(data_2$date), FUN=sum)
-head(data_3)
-plot(x = data_3$x)
+# Cumsum of total admission for each month:
+myocar2 <- aggregate(myocar$total_admission,
+                     by = list(myocar$month,
+                               myocar$year,
+                               myocar$facility_id),
+                     FUN = sum)
 
-# Example data (replace with your own data)
-set.seed(123)
-example <- matrix(rnorm(100, 0, 10), nrow = 10, ncol = 10)
-colnames(example) <- paste0("col", 1:10)
-rownames(example) <- paste0("row", 1:10)
+colnames(myocar2) <- c("month", "year", "facility_id", "admission")
 
-# Create a heatmap
-heatmap(example)  # Default heatmap with column and row names and a dendrogram
+# Hospitals of interest:
+san_borja <- subset(myocar2, facility_id == "11-100")
+sotero <- subset(myocar2, facility_id == "14-101")
+salvador <- subset(myocar2, facility_id == "12-100")
+san_jose <- subset(myocar2, facility_id == "09-100")
+san_juan <- subset(myocar2, facility_id == "10-100")
+
+# Graphic of the admission for myocardial infarction:
+par(mfrow = c(2, 3))
+plot(
+  san_borja$admission, 
+  main = "H. San Borja Arriarán", ylab = "Admissions", xlab = "Months",
+  col = "lightblue", pch = 19, axes = FALSE
+  ); axis(1, seq(0, 192, by=12)); axis(2)
+
+plot(
+  sotero$admission,
+  main = "H. Sótero del Río", ylab = "Admissions", xlab = "Months",
+  col = "lightblue", pch = 19, axes = FALSE
+  ); axis(1, seq(0, 192, by=12)); axis(2)
+plot(
+  salvador$admission,
+  main = "H. Del Salvador", ylab = "Admissions", xlab = "Months",
+  col = "lightblue", pch = 19, axes = FALSE
+  ); axis(1, seq(0, 192, by=12)); axis(2)
+plot(
+  san_jose$admission, 
+  main = "H. San José", ylab = "Admissions", xlab = "Months",
+  col = "lightblue", pch = 19, axes = FALSE
+); axis(1, seq(0, 192, by=12)); axis(2)
+plot(
+  san_juan$admission,
+  main = "H. San Jaun de Diós", ylab = "Admissions", xlab = "Months",
+  col = "lightblue", pch = 19, axes = FALSE
+  ); axis(1, seq(0, 192, by=12)); axis(2)
+# Obs: Discard data of San Borja hospitals
+
+# Heatmap:
+v1 <- salvador$admission
+v2 <- san_jose$admission
+v3 <- san_juan$admission
+v4 <- sotero$admission
+hosp_list <- list(v1, v2, v3, v4)
+
+mtx <- do.call(cbind, hosp_list)
+colnames(mtx) <- c("salvd", "sjose", "sjuan", "sote")
+
+heatmap(mtx, 
+        Colv = NA, Rowv = NA, 
+        scale = "column")
